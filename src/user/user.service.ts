57590972b6b609f7267bf22f36853106712fda44
Kauane Santos <kauanesantos.coding@gmail.com>
2024-03-05 20:10:16 -0300

    ♻️refactor: create validateUser

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,17 +7,23 @@ export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(data: CreateUserDTO) {
-    const existEmail = await this.prisma.user.findFirst({
-      where: { email: data.email },
-    });
-    if (existEmail) {
-      throw new UnauthorizedException('Email já cadastrado');
-    }
+    await this.validateUser(data);
 
     return this.prisma.user.create({ data });
   }
 
   async list() {
     return this.prisma.user.findMany();
+  }
+
+  async validateUser(data: any) {
+    console.log(data);
+    try {
+      await this.prisma.user.findFirst({
+        where: { NOT: data.email },
+      });
+    } catch (error) {
+      throw new UnauthorizedException('Email já cadastrado');
+    }
   }
 }
