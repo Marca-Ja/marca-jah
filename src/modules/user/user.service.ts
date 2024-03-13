ee666200b2d5766d62ae9ca6eb58fbd55e65dad9
Kauane Santos <kauanesantos.coding@gmail.com>
2024-03-13 19:54:09 -0300

    ♻️refactor: update validateUser

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, UnauthorizedException, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/infra/prisma.service';
 import { CreateUserDTO } from './DTO/create-user.dto';
 import { UpdatePatchUserDTO } from './DTO/update-patch-user.dto';
@@ -7,77 +11,104 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(private readonly prisma: PrismaService) {}
 
   async create(data: CreateUserDTO) {
     await this.validateUser(data);
 
-    const hash = await bcrypt.hash(data.password, 8)
+    const hash = await bcrypt.hash(data.password, 8);
 
-    data.password = hash
+    data.password = hash;
 
-    return this.prisma.user.create({data});
+    return this.prisma.user.create({ data });
   }
 
   async list() {
     return this.prisma.user.findMany();
   }
 
-  async update(id: string, data: UpdatePutUserDTO){
-    await this.exists(id);
+  async update(id: string, data: UpdatePutUserDTO) {
+    await this.validateUser(id);
 
-    const hash = await bcrypt.hash(data.password, 8)
-    
-    data.password = hash
+    const hash = await bcrypt.hash(data.password, 8);
+
+    data.password = hash;
 
     return this.prisma.user.update({
-      where: {id},
-      data
-    })
+      where: { id },
+      data,
+    });
   }
 
-  async updatePartial(id: string, {name, socialName, lastName, bornedAt, cellphone, email, password, postalCode, city, state, street, maritalState, receiveNews}: UpdatePatchUserDTO){
-    await this.exists(id);
+  async updatePartial(
+    id: string,
+    {
+      name,
+      socialName,
+      lastName,
+      bornedAt,
+      cellphone,
+      email,
+      password,
+      postalCode,
+      city,
+      state,
+      street,
+      maritalState,
+      receiveNews,
+    }: UpdatePatchUserDTO,
+  ) {
+    await this.validateUser(id);
 
     const data: any = {};
 
-  for (const [key, value] of Object.entries({ name, socialName, lastName, bornedAt, cellphone, email, password, postalCode, city, state, street, maritalState, receiveNews })) {
-    if (value) {
-      if (key === 'password' && typeof value === 'string') {
-        const hash = await bcrypt.hash(value, 8);
-        data[key] = hash;
-      } 
+    for (const [key, value] of Object.entries({
+      name,
+      socialName,
+      lastName,
+      bornedAt,
+      cellphone,
+      email,
+      password,
+      postalCode,
+      city,
+      state,
+      street,
+      maritalState,
+      receiveNews,
+    })) {
+      if (value) {
+        if (key === 'password' && typeof value === 'string') {
+          const hash = await bcrypt.hash(value, 8);
+          data[key] = hash;
+        }
 
-      data[key] = value;
+        data[key] = value;
+      }
     }
-  }
 
     return this.prisma.user.update({
-      where: {id},
-      data
-    })
+      where: { id },
+      data,
+    });
   }
 
   async validateUser(data: any) {
-    const user = await this.prisma.user.findFirst({
+    const userEmail = await this.prisma.user.findFirst({
       where: { email: data.email },
     });
-    if (user) {
+    if (userEmail) {
       throw new UnauthorizedException('Email já cadastrado');
     }
 
-    return true;
-  }
-
-  async exists(id: string) {
-    const user = await this.prisma.user.findFirst({
-      where: {
-        id,
-      },
+    const userId = await this.prisma.user.findFirst({
+      where: { id: data.id },
     });
 
-    if (!user) {
+    if (!userId) {
       throw new NotFoundException('User not found');
     }
+
+    return true;
   }
 }
