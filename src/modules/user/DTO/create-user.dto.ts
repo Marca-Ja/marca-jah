c64ab241644d73f4058f4be47b0cbc0a637f6fd2
Ana Beatriz Barreto <abbmr@hotmail.com>
2024-03-12 23:40:00 -0300

    :sparkles: feat: put&patch user +bcrypt +algumas validações no creat user

--- src/modules/user/DTO/create-user.dto.ts
+++ src/modules/user/DTO/create-user.dto.ts
@@ -5,7 +5,12 @@ import {
   IsString,
   IsStrongPassword,
   MinLength,
+  IsDate,
+  MinDate,
+  MaxDate,
+  Matches
 } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateUserDTO {
   @IsString()
@@ -23,8 +28,13 @@ export class CreateUserDTO {
 
   //   dependents: string[];
 
+  @Type(() => Date) 
+  @IsDate({ message: 'A data de nascimento deve ser uma data válida' })
+  @MinDate(getMinBirthDate(), { message: 'Você deve ter pelo menos 18 anos para se cadastrar' })
+  @MaxDate(new Date(), { message: 'A data de nascimento não pode ser no futuro' })
   bornedAt: string;
 
+  @Matches(/^(\+?\d{1,3})?[-.\s]?(\(\d{1,3}\))?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/, {message: 'Número de telefone inválido'})
   cellphone: string;
 
   @IsEmail()
@@ -39,6 +49,7 @@ export class CreateUserDTO {
   })
   password: string;
 
+  @Matches(/^\d{8}$/, { message: 'CEP inválido. O CEP deve conter 8 dígitos numéricos.' })
   postalCode: string;
 
   city: string;
@@ -52,4 +63,10 @@ export class CreateUserDTO {
   receiveNews: boolean;
 
   // medical_interest: string[];
+
+}
+
+function getMinBirthDate(): Date {
+  const today = new Date();
+  return new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
 }
