0f6f8f692fdff49f96352c795b67541d046fbc60
Daiane Bolzan <dainha85@gmail.com>
2024-03-28 20:50:55 -0300

    fix: update imports

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ROLES_KEY } from 'src/Decorators/roles.decorator';
+import { ROLES_KEY } from 'src/decorators/roles.decorator';
 import { Role } from '../enum/role.enum';
 
 @Injectable()
