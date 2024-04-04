27712891aa8ef4bfab42e5caf1915e78b9d3c878
Kauane Santos <kauanesantos.coding@gmail.com>
2024-04-04 18:28:29 -0300

    ♻️refactor: change appointmentId to doctorId + add exists func at findDoctor

--- src/modules/doctor/doctor.service.ts
+++ src/modules/doctor/doctor.service.ts
@@ -16,6 +16,8 @@ export class DoctorService {
   }
 
   async findDoctor(id: string) {
+    await this.exists(id);
+
     return this.prisma.doctor.findUnique({
       where: { id },
     });
@@ -39,43 +41,44 @@ export class DoctorService {
   }
 
   async exists(id: string) {
-    if (!(await this.prisma.doctor.count({ where: { id } }))) {
+    if (!(await this.prisma.doctor.findUnique({ where: { id } }))) {
       throw new NotFoundException('Médico não encontrado');
     }
   }
-  async findAllAppointmentsbyDoctor(appointmentId: string) {
-    const doctor = await this.prisma.doctor.findUnique({
-      where: { id: appointmentId },
-    });
-    if (!doctor) {
-      throw new NotFoundException('Médico não encontrado');
-    }
+
+  async findAllAppointmentsbyDoctor(doctorId: string) {
+    await this.exists(doctorId);
+
     const data = await this.prisma.appointment.findMany({
       where: {
-        doctorId: appointmentId,
+        doctorId,
       },
     });
 
     return data;
   }
-  async updateAppointment (appointmentId: string, status:UpdateDoctorAppointmentDto){
+  async updateAppointment(
+    appointmentId: string,
+    status: UpdateDoctorAppointmentDto,
+  ) {
     if (!status) {
-      throw new Error("Status DTO is undefined or not provided");
+      throw new Error('Status DTO is undefined or not provided');
     }
 
     const appointment = await this.prisma.appointment.findFirst({
-      where: {id: appointmentId}
-    })
-    if(!appointment){
-      throw new NotFoundException('Consulta não encontrada')
-    }
-
-
-    const updatedAppointment = await this.prisma.appointment.update({
-      data: {status: status.status},
       where: { id: appointmentId },
     });
-    return { message: 'Status da consulta atualizado com sucesso.', updatedAppointment  }
+    if (!appointment) {
+      throw new NotFoundException('Consulta não encontrada');
+    }
 
+    const updatedAppointment = await this.prisma.appointment.update({
+      data: { status: status.status },
+      where: { id: appointmentId },
+    });
+    return {
+      message: 'Status da consulta atualizado com sucesso.',
+      updatedAppointment,
+    };
   }
 }
